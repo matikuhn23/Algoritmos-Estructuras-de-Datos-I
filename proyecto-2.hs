deb4abc6101fde34acc1c48b8c26372e783dc5f1
Matías Ezequiel Kühn <70666239+matikuhn23@users.noreply.github.com>
2022-09-19 19:36:51 -0300

    Update proyecto-2.hs

--- proyecto-2.hs
+++ proyecto-2.hs
@@ -10,9 +10,9 @@ titulo Computacion = "Licenciatura en Ciencias de la Computacion"
 titulo Astronomia = "Licenciatura en Astronomia"
 titulo Mathaplic = "Licenciatura en Matemática Aplicada"
 
-data NotacionBasica = Do | Re | Mi | Fa | Sol | La | Si deriving (Eq, Ord, Show)
+data NotaBasica = Do | Re | Mi | Fa | Sol | La | Si deriving (Eq, Ord, Show)
 
-cifradoAmericano :: NotacionBasica -> Char
+cifradoAmericano :: NotaBasica -> Char
 
 cifradoAmericano Do = 'C'
 cifradoAmericano Re = 'D'
@@ -39,7 +39,7 @@ minimoElemento' [] = maxBound
 minimoElemento' (x:xs) = x `min` minimoElemento' xs
 
 -- Aplico la def de minimoElemento oara determinar la nota mas grave de la melod
-minimoElementon :: [NotacionBasica] -> NotacionBasica
+minimoElementon :: [NotaBasica] -> NotaBasica
 minimoElementon xs = minimoElemento xs
 
 --Ejercicio 4-------------------------------------------------------------------
@@ -78,9 +78,75 @@ cuantos_doc (Docente c1:xs) c | mismoCargo c c1 = 1 + cuantos_doc xs c
 cuantos_doc (_:xs) c = cuantos_doc xs c
 
 misma_per_carg :: Cargo -> Persona -> Bool
-misma_per_carg c (Docente c1) = mismoCargo c c1
-misma_per_carg c (_ c1) = False
+misma_per_carg c (Docente c1) | mismoCargo c c1 = True
+                              | otherwise = False
 
-flit_doc :: [Persona] -> Cargo -> Int
+filt_doc :: [Persona] -> Cargo -> Int
 filt_doc [] c = 0
-filt_doc (Docente c1:xs) c =
+filt_doc xs c = length (filter (misma_per_carg c) xs)
+
+{--
+ejercicio 5
+--}
+
+data Alteracion = Bemol | Sostenido | Natural deriving (Eq, Ord)
+data NotaMusical = Nota NotaBasica Alteracion deriving (Eq, Ord)
+
+sonido :: NotaBasica -> Int
+sonido Do = 1
+sonido Re = 3
+sonido Mi = 5
+sonido Fa = 6
+sonido Sol = 8
+sonido La = 10
+sonido Si = 12
+
+sonidoCromatico :: NotaMusical -> Int
+sonidoCromatico (Nota n Bemol) = (sonido n)-1
+sonidoCromatico (Nota n Sostenido) = (sonido n)+1
+sonidoCromatico (Nota n Natural) = sonido n
+
+instance Eq NotaMusical
+  where
+    Nota n v == Nota m b =
+      sonidoCromatico (Nota n v) == sonidoCromatico (Nota m b)
+
+instance Ord NotaMusical
+  where
+    Nota n v <= Nota m b =
+      sonidoCromatico (Nota n v) <= sonidoCromatico (Nota m b)
+
+{--
+ejercicio 6
+--}
+
+
+primerElemento :: [a] -> Maybe a
+primerElemento [] = Nothing
+primerElemento (x:xs) = Just (x)
+
+{--
+Ejercicio 7
+--}
+
+data Cola = Vacia | Encolada Persona Cola
+
+Encolada Decane (Encolada (Docente Titular) (Encolada (Docente Titular) Vacia))
+
+atender :: Cola -> Maybe Cola
+atender Vacia = Nothing
+atender Encolada p r = Just (r)
+
+encolar :: Persona -> Cola -> Cola
+encolar p Vacia = Encolada p Vacia
+encolar p (Encolada r cs) =  Encolada r (encolar p cs)
+
+busca :: Cola -> Cargo -> Maybe Persona
+busca Encolada Vacia c = Nothing
+busca (Encolada Docente c1 cs) c | mismoCargo c c1 = Just(Docente c1)
+                                 | otherwise = Busca cs c
+
+
+{--
+Ejercicio 8
+--}
