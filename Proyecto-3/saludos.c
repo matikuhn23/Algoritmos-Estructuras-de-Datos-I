815beca93da3af4651b6175c0423052eee9b756e
Matías Ezequiel Kühn <70666239+matikuhn23@users.noreply.github.com>
2022-10-17 00:27:39 -0300

    Create saludos.c

---
+++ Proyecto-3/saludos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void imprimeHola(void);
+void imprimeChau(void);
+
+int main(void){
+
+    imprimeHola();
+    imprimeChau();
+}
+
+void imprimeHola(void){
+    printf("Hola\n");
+    printf("Hola\n");
+    return;
+}
+
+void imprimeChau(void){
+    printf("Chau\n");
+    printf("Chau\n");
+    return;
+}
