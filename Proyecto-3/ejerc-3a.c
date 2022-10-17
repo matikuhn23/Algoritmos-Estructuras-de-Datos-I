0d274266261522e69ffa35577dcd9e8075c084ff
Matías Ezequiel Kühn <70666239+matikuhn23@users.noreply.github.com>
2022-10-17 00:16:48 -0300

    Create ejerc-3a.c

---
+++ Proyecto-3/ejerc-3a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+		int x;
+
+		printf("Ingrese un valor para x: \n");
+		scanf("%d", &x);
+
+		x = 5;
+		printf("La reasignación de x es: %d\n", x);
+
+		return 0;
+	}
