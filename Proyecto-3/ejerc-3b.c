0bdb1465c3dcf265c06d12fb097915756cf6c8ba
Matías Ezequiel Kühn <70666239+matikuhn23@users.noreply.github.com>
2022-10-17 00:17:12 -0300

    Create ejerc-3b.c

---
+++ Proyecto-3/ejerc-3b.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+		int x, y, i;
+
+	  /*
+	  Veo los resultados para los siguientes estados iniciales: x->2, y->5
+	  */
+		i = 1;
+		printf("Ingrese un valor para x \n");
+		scanf("%d", &x);
+		printf("Ingrese un valor para y \n");
+		scanf("%d", &y);
+
+		while (i<4) {
+
+			x = x + y;
+			y = y + y;
+
+			i = i+1;
+		}
+		printf("El resultado es %d \n", x);
+		printf("El resultado es %d \n", y);
+
+		return 0;
+	}
+
+/*
+El resultado es 37
+El resultado es 40
+*/
